b19fa287444672164300960d8ee217d4746756d1
anuja <anujag2020@gmail.com>
2026-01-08 18:35:17 +0530

    Face emotion recognition  using OpenCV. Face detection is implemented with Haar Cascade classifiers, and emotion labels are displayed using a temporally smoothed demo pipeline for learning purposes.

--- face_emotion_recognition/emotion_recognition_webcam.py
+++ face_emotion_recognition/emotion_recognition_webcam.py
@@ -7,7 +7,6 @@ from collections import deque
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
-
 # Open webcam
 cam = cv2.VideoCapture(0)
 
